ce4e63cecb6b22acd8754ee2b287f36bfbf21f8c
Simon <bardrabbit@gmail.com>
2020-11-26 20:46:57 -0500

    consume context in navbar with hooks

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -21,9 +21,3 @@ export class LanguageProvider extends Component {
     );
   }
 }
-
-export const withLanguageContext = (Component) => (props) => (
-  <LanguageContext.Consumer>
-    {(value) => <Component languageContext={value} {...props} />}
-  </LanguageContext.Consumer>
-);
